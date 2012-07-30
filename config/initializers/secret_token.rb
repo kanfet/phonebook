a2fa0e846419804ec81179f22929907f2000608f
kanfet <byte.developer@gmail.com>
2012-07-30 17:40:08 +0600

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phonebook::Application.config.secret_token = '47fb2ed84c758efa0c3d95938f1b424fbfb29d94d7d42994467a58c1bf1bac2db27e69c4005a2a0c956952544e24f3b06c5ccb6f18b8b2d3b6f78b6a0248a5a8'
